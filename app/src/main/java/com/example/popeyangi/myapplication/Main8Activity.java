ff722a9bdaca92e579ed3c5476c6027d908d9ddc
Lemoner <yangqi@email.com>
2016-11-14 15:57:09 +0900

    Revert "GUI"
    This reverts commit b116695e699697ccf9d26028f892310fc9515cdc.

--- app/src/main/java/com/example/popeyangi/myapplication/Main8Activity.java
+++
@@ -1,9 +0,0 @@
-package com.example.popeyangi.myapplication;
-
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-
-public class Main8Activity extends AppCompatActivity {
-    Intent intent = null;
-}
